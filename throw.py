c43bd5532559fafa3d1022a28af235e77295af6b
Jeremy Hsu <jeremyhsu1@gmail.com>
2023-11-30 15:49:52 -0500

    Pass in port number through command line

--- throw.py
+++ throw.py
@@ -1,3 +1,4 @@
+import argparse
 import grpc
 import concurrent.futures as futures
 import logging
@@ -5,11 +6,16 @@ import fedscale.cloud.config_parser as parser
 
 from fedscale.cloud.gossip.gossip_executor import Executor
 
+
 def create_server(port: int):
     e = Executor(args=parser.args, client_id=port)
     e.run()
 
-if __name__ == "__main__": 
-    create_server(1)
+
+if __name__ == "__main__":
+    aparser = argparse.ArgumentParser(description="Run the server.")
+    aparser.add_argument('port', type=int, help='The port of the server.')
+    args = aparser.parse_args()
+    create_server(args.port)
 
 
